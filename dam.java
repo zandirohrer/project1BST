c8941544221de841cf2e18dbb4d58d12a34a28f8
zandirohrer <rhrale001@myuct.ac.za>
2018-03-13 19:56:39 +0200

    program

---
+++ dam.java
@@ -0,0 +1,17 @@
+//Alexander Rohrer
+
+public class dam{
+    String name;
+    String Fsc;
+    String level;
+    
+    public dam(String n, String x, String y){
+        name = n;
+        Fsc = x;
+        level = y;
+    }
+    
+    public string toString(){
+        return "Dam name: " + name + "FSC: " + Fsc + "level: " + level;
+    }
+}
