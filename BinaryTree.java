5faf7d166322716ee351337d4f4e8450312243c9
zandirohrer <rhrale001@myuct.ac.za>
2018-03-14 16:13:23 +0200

    initial Tree

---
+++ BinaryTree.java
@@ -0,0 +1,32 @@
+//Alexander Rohrer
+class BinaryTree {
+	public static  Node root;
+	public BinaryTree(){
+            this.root = null;
+        }
+        
+        public void insert(dam id){
+		Node newNode = new Node(id);
+		if(root==null){
+			root = newNode;
+			return;
+		}
+		Node current = root;
+		Node parent = null;
+		while(true){
+			parent = current;
+			if(id.levelInt<current.data){				
+				current = current.left;
+				if(current==null){
+					parent.left = newNode;
+					return;
+				}
+			}else{
+				current = current.right;
+				if(current==null){
+					parent.right = newNode;
+					return;
+				}
+			}
+		}
+}
